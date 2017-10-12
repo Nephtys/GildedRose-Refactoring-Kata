5d105d9a4c802e9e01a0b31691e63d159facba16
Stefanie Loiseleur <sloiseleur@ippon.fr>
2017-10-12 22:33:32 +0200

    Move logic to domain

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,63 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateQuality(item);
-        }
-    }
-
-    private void updateQuality(Item item) {
-
-        if (SULFURAS.equals(item.name)) {
-            return;
-        }
-
-        if (AGED_BRIE.equals(item.name)) {
-            if (item.quality < 50) {
-                item.quality++;
-            }
-
-            item.sellIn--;
-
-            if (item.sellIn < 0 && item.quality < 50) {
-                item.quality++;
-            }
-            return;
-        }
-
-        if (BACKSTAGE_PASS.equals(item.name)) {
-            if (item.quality < 50) {
-                item.quality++;
-
-                if (item.sellIn < 11 && item.quality < 50) {
-                    item.quality++;
-                }
-
-                if (item.sellIn < 6 && item.quality < 50) {
-                    item.quality++;
-                }
-            }
-
-            item.sellIn--;
-
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            }
-            return;
-        }
-
-
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-
+            item.updateQuality();
         }
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 public class Item {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
 
     public String name;
 
@@ -14,8 +17,64 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    public void updateQuality() {
+
+        if (SULFURAS.equals(this.name)) {
+            return;
+        }
+
+        if (AGED_BRIE.equals(this.name)) {
+            if (this.quality < 50) {
+                this.quality++;
+            }
+
+            this.sellIn--;
+
+            if (this.sellIn < 0 && this.quality < 50) {
+                this.quality++;
+            }
+            return;
+        }
+
+        if (BACKSTAGE_PASS.equals(this.name)) {
+            if (this.quality < 50) {
+                this.quality++;
+
+                if (this.sellIn < 11 && this.quality < 50) {
+                    this.quality++;
+                }
+
+                if (this.sellIn < 6 && this.quality < 50) {
+                    this.quality++;
+                }
+            }
+
+            this.sellIn--;
+
+            if (this.sellIn < 0) {
+                this.quality = 0;
+            }
+            return;
+        }
+
+
+        if (this.quality > 0) {
+            this.quality = this.quality - 1;
+        }
+
+
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0) {
+            if (this.quality > 0) {
+                this.quality = this.quality - 1;
+            }
+
+        }
     }
 }
