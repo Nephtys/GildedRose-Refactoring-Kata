65058ba15d7d80ecc7e191654c3cac757cd2da84
Stefanie Loiseleur <sloiseleur@ippon.fr>
2017-10-12 22:47:41 +0200

    Introduce item specific cases sub classes

--- src/main/java/com/gildedrose/ItemFactory.java
+++ src/main/java/com/gildedrose/ItemFactory.java
@@ -4,6 +4,15 @@ public final class ItemFactory {
     private ItemFactory() {}
 
     public static Item build(String name, int sellIn, int quality) {
-        return new Item(name, sellIn, quality);
+        switch(name) {
+            case Item.AGED_BRIE:
+                return new AgedBrieItem(name, sellIn, quality);
+            case Item.BACKSTAGE_PASS:
+                return new BackstagePassItem(name, sellIn, quality);
+            case Item.SULFURAS:
+                return new SulfurasItem(name, sellIn, quality);
+            default:
+                return new Item(name, sellIn, quality);
+        }
     }
 }
