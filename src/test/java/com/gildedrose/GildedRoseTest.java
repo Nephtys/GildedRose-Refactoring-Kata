1866f9488e5ca8d7b7a908fd2f3e4d8022fb4d6d
Stefanie Loiseleur <sloiseleur@ippon.fr>
2017-10-12 22:58:50 +0200

    Add failing tests for the conjured item case

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,7 +10,8 @@ public class GildedRoseTest {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    
+    private static final String CONJURED = "Conjured Mana Cake";
+
     private static final String QUALITY_FIELD = "quality";
     private static final String SELL_IN_FIELD = "sellIn";
 
@@ -216,6 +217,60 @@ public class GildedRoseTest {
                 ItemFactory.build(BACKSTAGE_PASS, 5, 0),
                 ItemFactory.build(BACKSTAGE_PASS, 0, 0),
                 ItemFactory.build(BACKSTAGE_PASS, -1, 0),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items).extracting(SELL_IN_FIELD).containsExactly(4, -1, -2);
+    }
+
+    // CONJURED ITEM
+
+    @Test
+    public void the_quality_of_a_conjured_item_should_decrease_by_2_before_sell_date() {
+        Item[] items = new Item[] {
+                ItemFactory.build(CONJURED, 5, 10),
+                ItemFactory.build(CONJURED, 1, 10),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items).extracting(QUALITY_FIELD).containsOnly(8);
+    }
+
+    @Test
+    public void the_quality_of_a_conjured_item_should_decrease_by_4_after_sell_date() {
+        Item[] items = new Item[] {
+                ItemFactory.build(CONJURED, -5, 10),
+                ItemFactory.build(CONJURED, -1, 10),
+                ItemFactory.build(CONJURED, 0, 10),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items).extracting(QUALITY_FIELD).containsOnly(6);
+    }
+
+    @Test
+    public void the_quality_of_a_conjured_item_should_not_be_less_than_0() {
+        Item[] items = new Item[] {
+                ItemFactory.build(CONJURED, 5, 2),
+                ItemFactory.build(CONJURED, 5, 1),
+                ItemFactory.build(CONJURED, 5, 0),
+                ItemFactory.build(CONJURED, -1, 4),
+                ItemFactory.build(CONJURED, -1, 3),
+                ItemFactory.build(CONJURED, -1, 2),
+                ItemFactory.build(CONJURED, -1, 1),
+                ItemFactory.build(CONJURED, -1, 0),
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items).extracting(QUALITY_FIELD).containsOnly(0);
+    }
+
+    @Test
+    public void the_sellIn_value_of_a_conjured_item_should_decrease() {
+        Item[] items = new Item[] {
+                ItemFactory.build(CONJURED, 5, 0),
+                ItemFactory.build(CONJURED, 0, 0),
+                ItemFactory.build(CONJURED, -1, 0),
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
