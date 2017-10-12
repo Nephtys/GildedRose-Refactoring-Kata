61fb9da59cf4056fb8fae47a6a69f7164435f3f0
Stefanie Loiseleur <sloiseleur@ippon.fr>
2017-10-12 22:30:30 +0200

    Extract backstage pass case

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -35,41 +35,40 @@ class GildedRose {
             return;
         }
 
-
-        if (!BACKSTAGE_PASS.equals(item.name)) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        } else {
+        if (BACKSTAGE_PASS.equals(item.name)) {
             if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                item.quality++;
 
-                if (BACKSTAGE_PASS.equals(item.name)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 11 && item.quality < 50) {
+                    item.quality++;
+                }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 6 && item.quality < 50) {
+                    item.quality++;
                 }
             }
+
+            item.sellIn--;
+
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
+            return;
         }
+
+
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
 
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            if (!BACKSTAGE_PASS.equals(item.name)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                item.quality = 0;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
+
         }
     }
 }
