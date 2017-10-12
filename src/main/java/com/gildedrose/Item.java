4bba3e21487172baeb195501c14dbaf0f0bd843b
Stefanie Loiseleur <sloiseleur@ippon.fr>
2017-10-12 22:53:01 +0200

    Some more cleanup

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -24,16 +24,13 @@ public class Item {
 
     public void updateQuality() {
         if (this.quality > 0) {
-            this.quality = this.quality - 1;
+            this.quality--;
         }
 
-        this.sellIn = this.sellIn - 1;
+        this.sellIn--;
 
-        if (this.sellIn < 0) {
-            if (this.quality > 0) {
-                this.quality = this.quality - 1;
-            }
-
+        if (this.sellIn < 0 && this.quality > 0) {
+            this.quality--;
         }
     }
 }
